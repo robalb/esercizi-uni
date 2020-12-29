e123d7d3706fea9b4b33c36ec04e87bd5dc3cb3c
robalb <alberto.ventafridda@gmail.com>
2020-12-29 01:08:40 +0100

    minor tweaks to template

--- template/Main.java
+++ template/Main.java
@@ -3,10 +3,22 @@ import java.io.*;
 
 /*
  * Esercizi uni
+ * use as cli application
  * @author Alberto ventafridda
  * Esercizio
  */
-public static void main(String[] args){
-  //Scanner scanner = new Scanner(System.in);
-  //scanner.next[Int|Double|String]();
+public class XX{
+  public static void main(String[] args){
+    //Scanner scanner = new Scanner(System.in);
+    //scanner.next[Int|Double]();
+    // try{
+    //   XX app = new XX();
+    //   app.run();
+    // }catch(Exception e){
+    //   System.out.println(e.toString());
+    // }
+  }
+
+  // private void run() throws Exception{
+  // }
 }
